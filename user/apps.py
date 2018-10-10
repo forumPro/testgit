fca52a66b61fd23c898f10fe28ebf61577e2e97b
gaozhenliang <1336188490@qq.com>
2018-10-10 08:48:40 +0800

    add user module

--- user/apps.py
+++ user/apps.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
-    name = 'user'
+class UsersConfig(AppConfig):
+    name = 'users'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    SEX = (
+        ('M','男性'),
+        ('F','女性'),
+        ('S','保密')
+    )
+    nickname = models.CharField(max_length=32,unique=True)
+    password = models.CharField(max_length=128)
+    icon = models.ImageField()
+    age = models.IntegerField(default=18)
+    sex = models.CharField(choices=SEX,max_length=20)
+
+
+
